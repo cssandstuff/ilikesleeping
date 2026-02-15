c7b599834169c192eb95edc6d6cf22d154a0fc72
Tim C <tim@cssandstuff.com>
2026-02-15 21:07:20 +1300

    site name

--- src/_data/meta.mjs
+++ src/_data/meta.mjs
@@ -1,5 +1,5 @@
 export default {
   url: Deno.env.get("Host") || "http://localhost:8080",
-  siteName: "11ty deno htmx",
+  siteName: "Hanako Clulow - Illustrator",
   siteDescription: "A quick way to get started with Eleventy and deno deploy (also using HTMX)",
 };
